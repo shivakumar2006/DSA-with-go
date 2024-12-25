ec164d3cb672265689d5d24fb2867c482a5e739d
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-25 23:36:35 +0530

    complete

---
+++ max_sum/maxSum.go
@@ -0,0 +1,36 @@
+// max_sum_of_pair_with_equal_sum_of_digit
+
+package main
+
+import "fmt"
+
+func main() {
+	array := []int{18, 43, 36, 13, 7}
+	fmt.Println(maximumCard(array))
+}
+
+func maximumCard(array []int) int {
+	hashMap := make(map[int]int)
+	result := -1
+	for i := 0; i < len(array); i++ {
+		s := sum(array[i])
+		if value, ok := hashMap[s]; ok {
+			result = max(result, array[i]+value)
+			if array[i] > value {
+				hashMap[s] = array[i]
+			}
+		} else {
+			hashMap[s] = array[i]
+		}
+	}
+	return result
+}
+
+func sum(n int) int {
+	target := 0
+	if n > 0 {
+		target = n % 10
+		n = n / 10
+	}
+	return target
+}
