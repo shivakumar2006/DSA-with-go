65cface343710135f7395ced1c3f87a083361dad
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-26 00:35:00 +0530

    solved

--- rotate_array/rotateArr.go
+++ rotate_array/rotateArr.go
@@ -1,3 +1,30 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	array := []int{1, 2, 3, 4, 5}
+// 	fmt.Println(rotateArray(array, 3))
+// }
+
+// func rotateArray(array []int, d int) []int {
+// 	n := len(array)
+// 	rev(array, 0, d-1)
+// 	rev(array, d, n-1)
+// 	rev(array, 0, n-1)
+// 	return array
+// }
+
+// func rev(array []int, l int, r int) {
+// 	for l < r {
+// 		temp := array[l]
+// 		array[l] = array[r]
+// 		array[r] = temp
+// 		l++
+// 		r--
+// 	}
+// }
+
 package main
 
 import "fmt"
