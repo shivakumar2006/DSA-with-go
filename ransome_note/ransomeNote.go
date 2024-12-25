f7dd4126a3bc8e89e9c9d14b2b2a464d852f2751
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-25 16:04:55 +0530

    commit

--- ransome_note/ransomeNote.go
+++ ransome_note/ransomeNote.go
@@ -1,3 +1,29 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ransomeNote := "aa"
+// 	magazine := "aab"
+// 	fmt.Println(canConstruct(ransomeNote, magazine))
+// }
+
+// func canConstruct(ransomeNote string, magazine string) bool {
+// 	charSet := [26]int{}
+
+// 	for i := 0; i < len(magazine); i++ {
+// 		charSet[magazine[i]-'a']++
+// 	}
+
+// 	for i := 0; i < len(ransomeNote); i++ {
+// 		charSet[ransomeNote[i]-'a']--
+// 		if charSet[ransomeNote[i]-'a'] < 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
 package main
 
 import "fmt"
@@ -10,7 +36,6 @@ func main() {
 
 func canConstruct(ransomeNote string, magazine string) bool {
 	charSet := [26]int{}
-
 	for i := 0; i < len(magazine); i++ {
 		charSet[magazine[i]-'a']++
 	}
