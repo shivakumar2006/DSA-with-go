fe2d149836e3bbb226115eb1f119fb95ddb25a8d
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-25 13:45:53 +0530

    commit

---
+++ ransomeNote.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	ransomeNote := "aa"
+	magazine := "aab"
+	fmt.Println(canConstruct(ransomeNote, magazine))
+}
+
+func canConstruct(ransomeNote string, magazine string) bool {
+	charSet := [26]int{}
+
+	for i := 0; i < len(magazine); i++ {
+		charSet[magazine[i]-'a']++
+	}
+
+	for i := 0; i < len(ransomeNote); i++ {
+		charSet[ransomeNote[i]-'a']--
+		if charSet[ransomeNote[i]-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ ransome_note/go.mod
@@ -0,0 +1,3 @@
+module github.com/yourusername/ransome_note
+
+go 1.23.4
