8a89873a6f1745c4fde3108c5261742df6344229
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-28 05:03:48 +0530

    solved

---
+++ jewels_stones/js.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	jewels := "aA"
+	stones := "aAAbbbbb"
+	fmt.Println(check(jewels, stones))
+}
+
+func check(jewels string, stones string) int {
+	hashMap := make(map[byte]bool)
+
+	for i := 0; i < len(jewels); i++ {
+		hashMap[jewels[i]] = true
+	}
+
+	result := 0
+	for i := 0; i < len(stones); i++ {
+		if hashMap[stones[i]] {
+			result++
+		}
+	}
+	return result
+}
