12a7e82377fbea62685e916e1a47ab0e1f97cb0a
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-27 16:14:46 +0530

    solved

--- find_difference/findDiff.go
+++ find_difference/findDiff.go
@@ -1,3 +1,22 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	n := 10
+// 	m := 2
+// 	fmt.Println((findDifference(n, m)))
+// }
+
+// func findDifference(n int, m int) int {
+// 	x := n / m
+// 	num2 := m * x * (x + 1) / 2
+// 	num1 := n * (n + 1) / 2
+// 	return num1 - num2
+// }
+
+// another way of solve this through loop
+
 package main
 
 import "fmt"
@@ -5,12 +24,17 @@ import "fmt"
 func main() {
 	n := 10
 	m := 2
-	fmt.Println((findDifference(n, m)))
+	fmt.Println(findDifference(n, m))
 }
 
 func findDifference(n int, m int) int {
-	x := n / m
-	num2 := m * x * (x + 1) / 2
-	num1 := n * (n + 1) / 2
+	num1 := 0
+	num2 := 0
+	for i := 0; i <= n; i++ {
+		num1 += i
+	}
+	for i := m; i <= n; i += m {
+		num2 += i
+	}
 	return num1 - num2
 }
