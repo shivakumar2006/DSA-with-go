e5cdbb8d002531c17ab65d996abd254133a6f016
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-27 22:13:00 +0530

    solved

--- remove_duplicates/removeDupli.go
+++ remove_duplicates/removeDupli.go
@@ -1,22 +1,22 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	nums := []int{1, 1, 2, 3, 4, 4, 5}
-	fmt.Println(removeDuplicate(nums))
-}
+// func main() {
+// 	nums := []int{1, 1, 2, 3, 4, 4, 5}
+// 	fmt.Println(removeDuplicate(nums))
+// }
 
-func removeDuplicate(nums []int) int {
-	unique := 1
-	for i := 1; i < len(nums); i++ {
-		if nums[i] != nums[i-1] {
-			unique += 1
-			nums[unique-1] = nums[i]
-		}
-	}
-	return unique
-}
+// func removeDuplicate(nums []int) int {
+// 	unique := 1
+// 	for i := 1; i < len(nums); i++ {
+// 		if nums[i] != nums[i-1] {
+// 			unique += 1
+// 			nums[unique-1] = nums[i]
+// 		}
+// 	}
+// 	return unique
+// }
 
 // another way of solving this with the output len of nums as well as array
 
@@ -41,3 +41,25 @@ func removeDuplicate(nums []int) int {
 // 	}
 // 	return unique
 // }
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 1, 2, 3, 4, 4, 5}
+	length := removeDuplicate(nums)
+	fmt.Println("total numbers : ", length)
+	fmt.Println("numbers : ", nums[:length])
+}
+
+func removeDuplicate(nums []int) int {
+	unique := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			unique += 1
+			nums[unique-1] = nums[i]
+		}
+	}
+	return unique
+}
