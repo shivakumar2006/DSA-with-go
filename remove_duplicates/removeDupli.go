64d411eed290de299d51ef62c5ca35809edd09bd
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-27 16:43:40 +0530

    solved

--- remove_duplicates/removeDupli.go
+++ remove_duplicates/removeDupli.go
@@ -1,10 +1,34 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	nums := []int{1, 1, 2, 3, 4, 4, 5}
+// 	fmt.Println(removeDuplicate(nums))
+// }
+
+// func removeDuplicate(nums []int) int {
+// 	unique := 1
+// 	for i := 1; i < len(nums); i++ {
+// 		if nums[i] != nums[i-1] {
+// 			unique += 1
+// 			nums[unique-1] = nums[i]
+// 		}
+// 	}
+// 	return unique
+// }
+
+// another way of solving this with the output len of nums as well as array
+
 package main
 
 import "fmt"
 
 func main() {
 	nums := []int{1, 1, 2, 3, 4, 4, 5}
-	fmt.Println(removeDuplicate(nums))
+	length := removeDuplicate(nums)
+	fmt.Println("total numbers : ", length)
+	fmt.Println("unique elements : ", nums[:length])
 }
 
 func removeDuplicate(nums []int) int {
