44bc1c0d4364646180da5c3836661a284322f395
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-28 05:39:03 +0530

    solved

--- occurence/occurence.go
+++ occurence/occurence.go
@@ -1,4 +1,34 @@
-// check if all number have equal number of occurrence
+// // check if all number have equal number of occurrence
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	s := "abacbc"
+// 	fmt.Println(areEqualOccurrence(s))
+// }
+
+// func areEqualOccurrence(s string) bool {
+// 	charSet := [26]int{}
+// 	x := 0
+
+// 	for i := 0; i < len(s); i++ {
+// 		charSet[s[i]-'a']++
+// 	}
+
+// 	for i := 0; i < len(charSet); i++ {
+// 		if charSet[i] != 0 {
+// 			if x == 0 {
+// 				x = charSet[i]
+// 			} else if charSet[i] != x {
+// 				return false
+// 			}
+// 		}
+
+// 	}
+// 	return true
+// }
 
 package main
 
@@ -25,7 +55,6 @@ func areEqualOccurrence(s string) bool {
 				return false
 			}
 		}
-
 	}
 	return true
 }
