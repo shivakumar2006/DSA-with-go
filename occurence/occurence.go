7617644721038f746429261b1b96aedff0d943ad
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-28 05:44:34 +0530

    solved

--- occurence/occurence.go
+++ occurence/occurence.go
@@ -1,35 +1,5 @@
 // // check if all number have equal number of occurrence
 
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	s := "abacbc"
-// 	fmt.Println(areEqualOccurrence(s))
-// }
-
-// func areEqualOccurrence(s string) bool {
-// 	charSet := [26]int{}
-// 	x := 0
-
-// 	for i := 0; i < len(s); i++ {
-// 		charSet[s[i]-'a']++
-// 	}
-
-// 	for i := 0; i < len(charSet); i++ {
-// 		if charSet[i] != 0 {
-// 			if x == 0 {
-// 				x = charSet[i]
-// 			} else if charSet[i] != x {
-// 				return false
-// 			}
-// 		}
-
-// 	}
-// 	return true
-// }
-
 package main
 
 import "fmt"
@@ -55,6 +25,63 @@ func areEqualOccurrence(s string) bool {
 				return false
 			}
 		}
+
 	}
 	return true
 }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	s := "abacbc"
+// 	fmt.Println(areEqualOccurrence(s))
+// }
+
+// func areEqualOccurrence(s string) bool {
+// 	charSet := [26]int{}
+// 	x := 0
+
+// 	for i := 0; i < len(s); i++ {
+// 		charSet[s[i]-'a']++
+// 	}
+
+// 	for i := 0; i < len(charSet); i++ {
+// 		if charSet[i] != 0 {
+// 			if x == 0 {
+// 				x = charSet[i]
+// 			} else if charSet[i] != x {
+// 				return false
+// 			}
+// 		}
+// 	}
+// 	return true
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	s := "abacbc"
+// 	fmt.Println(areEqualOccurrence(s))
+// }
+
+// func areEqualOccurrence(s string) bool {
+// 	charSet := [26]int{}
+// 	x := 0
+// 	for i := 0; i < len(s); i++ {
+// 		charSet[s[i]-'a']++
+// 	}
+// 	for i := 0; i < len(charSet); i++ {
+// 		if charSet[i] != 0 {
+// 			if x == 0 {
+// 				x = charSet[i]
+// 			} else if charSet[i] != x {
+// 				return false
+// 			}
+// 		}
+// 	}
+// 	return true
+// }
