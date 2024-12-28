ae4d5e89baa9b7c7890b1f7cd9b20a8f474a055e
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-28 06:01:31 +0530

    commit

--- binary_to_decimal/btd.go
+++ binary_to_decimal/btd.go
@@ -1,3 +1,19 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"strconv"
+// )
+
+// func main() {
+// 	binaryString := "0101"
+// 	decimalValue, err := strconv.ParseInt(binaryString, 2, 0)
+// 	if err != nil {
+// 		fmt.Println("Error : ", err)
+// 	}
+// 	fmt.Println(decimalValue)
+// }
+
 package main
 
 import (
@@ -6,7 +22,7 @@ import (
 )
 
 func main() {
-	binaryString := "0101"
+	binaryString := "10010010010011"
 	decimalValue, err := strconv.ParseInt(binaryString, 2, 0)
 	if err != nil {
 		fmt.Println("Error : ", err)
