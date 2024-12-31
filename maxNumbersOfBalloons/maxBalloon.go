29243b9b7216fb5b987919ab35a9d2b84624afa7
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-31 17:35:42 +0530

    solved

--- maxNumbersOfBalloons/maxBalloon.go
+++ maxNumbersOfBalloons/maxBalloon.go
@@ -1,3 +1,41 @@
+// package main
+
+// import "fmt"
+
+// func min(a int, b int) int {
+// 	if a < b {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// func main() {
+// 	input := "nlaebolko"
+// 	fmt.Println(maxNumbersOfBalloons(input))
+// }
+
+// func maxNumbersOfBalloons(input string) int {
+// 	b, a, l, o, n := 0, 0, 0, 0, 0
+// 	for i := 0; i < len(input); i++ {
+// 		if input[i] == 'b' {
+// 			b++
+// 		} else if input[i] == 'a' {
+// 			a++
+// 		} else if input[i] == 'l' {
+// 			l++
+// 		} else if input[i] == 'o' {
+// 			o++
+// 		} else if input[i] == 'n' {
+// 			n++
+// 		}
+// 	}
+
+// 	l /= 2
+// 	o /= 2
+
+// 	return min(b, min(a, min(l, min(o, n))))
+// }
+
 package main
 
 import "fmt"
@@ -10,7 +48,7 @@ func min(a int, b int) int {
 }
 
 func main() {
-	input := "nlaebolko"
+	input := "loobalnxballpoon"
 	fmt.Println(maxNumbersOfBalloons(input))
 }
 
@@ -29,7 +67,6 @@ func maxNumbersOfBalloons(input string) int {
 			n++
 		}
 	}
-
 	l /= 2
 	o /= 2
 
