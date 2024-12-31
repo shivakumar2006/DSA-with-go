d269284433579839b178d5e5264b1cb74389d1ed
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-31 16:28:17 +0530

    solved

--- intersection_of_multipleArray/mulArr.go
+++ intersection_of_multipleArray/mulArr.go
@@ -1,31 +1,79 @@
 // intersection of multiple array
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	nums := [][]int{
+// 		{3, 1, 2, 4, 5},
+// 		{1, 2, 3, 4},
+// 		{3, 4, 5, 6},
+// 	}
+// 	fmt.Println(intersection(nums))
+// }
+
+// func intersection(nums [][]int) []int {
+// 	hashMap := make(map[int]int)
+// 	n := len(nums)
+// 	for i := 0; i < n; i++ {
+// 		for j := 0; j < len(nums[i]); j++ {
+// 			hashMap[nums[i][j]]++
+// 		}
+// 	}
+// 	result := []int{}
+// 	for nums, count := range hashMap {
+// 		if count == 0 {
+// 			result = append(result, nums)
+// 		}
+// 	}
+// 	return result
+// }
+
+//DEBUG CODE
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	nums := [][]int{
+// 		{3, 1, 2, 4, 5},
+// 		{1, 2, 3, 4},
+// 		{3, 4, 5, 6},
+// 	}
+// 	fmt.Println(intersection(nums))
+// }
+
+// func intersection(nums [][]int) []int {
+// 	hashMap := make(map[int]int)
+// 	n := len(nums)
+
+// 	fmt.Println("input arrays : ", nums)
+
+// 	for i := 0; i < n; i++ {
+// 		for j := 0; j < len(nums[i]); j++ {
+// 			hashMap[nums[i][j]]++
+// 		}
+// 	}
+
+// 	fmt.Println("hashMap : ", hashMap)
+
+// 	result := []int{}
+// 	for num, count := range hashMap {
+// 		fmt.Printf("Number: %d, Count: %d \n", num, count)
+// 		if count == n {
+// 			result = append(result, num)
+// 		}
+// 	}
+
+// 	fmt.Println("Result", result)
+
+// 	return result
+// }
+
+
 package main
 
 import "fmt"
 
-func main() {
-	nums := [][]int{
-		{3, 1, 2, 4, 5},
-		{1, 2, 3, 4},
-		{3, 4, 5, 6},
-	}
-	fmt.Println(intersection(nums))
-}
-
-func intersection(nums [][]int) []int {
-	hashMap := make(map[int]int)
-	n := len(nums)
-	for i := 0; i < n; i++ {
-		for j := 0; j < len(nums[i]); j++ {
-			hashMap[nums[i][j]]++
-		}
-	}
-	result := []int{}
-	for nums, count := range hashMap {
-		if count == 0 {
-			result = append(result, nums)
-		}
-	}
-	return result
-}
+func 
