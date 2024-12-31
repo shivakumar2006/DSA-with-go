e6c4a48b39fe482f9e90d97435d7ad822c49e093
shivakumar2006 <official.shivakumar06@gmail.com>
2024-12-31 22:24:07 +0530

    solved

--- maxNumbersOfBalloons/maxBalloon.go
+++ maxNumbersOfBalloons/maxBalloon.go
@@ -36,6 +36,43 @@
 // 	return min(b, min(a, min(l, min(o, n))))
 // }
 
+// package main
+
+// import "fmt"
+
+// func min(a int, b int) int {
+// 	if a < b {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// func main() {
+// 	input := "loobalnxballpoon"
+// 	fmt.Println(maxNumbersOfBalloons(input))
+// }
+
+// func maxNumbersOfBalloons(input string) int {
+// 	b, a, l, o, n := 0, 0, 0, 0, 0
+// 	for i := 0; i < len(input); i++ {
+// 		if input[i] == 'b' {
+// 			b++
+// 		} else if input[i] == 'a' {
+// 			a++
+// 		} else if input[i] == 'l' {
+// 			l++
+// 		} else if input[i] == 'o' {
+// 			o++
+// 		} else if input[i] == 'n' {
+// 			n++
+// 		}
+// 	}
+// 	l /= 2
+// 	o /= 2
+
+// 	return min(b, min(a, min(l, min(o, n))))
+// }
+
 package main
 
 import "fmt"
@@ -48,22 +85,22 @@ func min(a int, b int) int {
 }
 
 func main() {
-	input := "loobalnxballpoon"
-	fmt.Println(maxNumbersOfBalloons(input))
+	s := "dfrebjattljyyljgyogyoccn"
+	fmt.Println(maxNumberOfBalloon(s))
 }
 
-func maxNumbersOfBalloons(input string) int {
+func maxNumberOfBalloon(s string) int {
 	b, a, l, o, n := 0, 0, 0, 0, 0
-	for i := 0; i < len(input); i++ {
-		if input[i] == 'b' {
+	for i := o; i < len(s); i++ {
+		if s[i] == 'b' {
 			b++
-		} else if input[i] == 'a' {
+		} else if s[i] == 'a' {
 			a++
-		} else if input[i] == 'l' {
+		} else if s[i] == 'l' {
 			l++
-		} else if input[i] == 'o' {
+		} else if s[i] == 'o' {
 			o++
-		} else if input[i] == 'n' {
+		} else if s[i] == 'n' {
 			n++
 		}
 	}
