aace99514e0cc35ca846e418c1f04b7e3f175936
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-10 14:51:31 +0530

    solved

--- ransome_note/ransomeNote.go
+++ ransome_note/ransomeNote.go
@@ -75,6 +75,32 @@
 // 	return true
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ransomeNote := "aa"
+// 	magazine := "aab"
+// 	fmt.Println(canConstruct(ransomeNote, magazine))
+// }
+
+// func canConstruct(ransomeNote string, magazine string) bool {
+// 	charSet := [26]int{}
+
+// 	for i := 0; i < len(magazine); i++ {
+// 		charSet[magazine[i]-'a']++
+// 	}
+
+// 	for i := 0; i < len(ransomeNote); i++ {
+// 		charSet[ransomeNote[i]-'a']--
+// 		if charSet[ransomeNote[i]-'a'] < 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
 package main
 
 import "fmt"
