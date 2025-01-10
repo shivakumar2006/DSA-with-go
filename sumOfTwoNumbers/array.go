d2a879b3e6952be1c953fc832b517cdd97028a8f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-10 23:12:47 +0530

    solved

--- sumOfTwoNumbers/array.go
+++ sumOfTwoNumbers/array.go
@@ -31,6 +31,36 @@
 // 	return nil
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	nums := []int{2, 3, 5, 7, 11}
+// 	target := 10
+// 	result := twoSum(nums, target)
+// 	if result == nil {
+// 		fmt.Println("there is not any two pair of array whose sum is equal to the target...")
+// 	} else {
+// 		fmt.Println("Indices of numbers : ", result)
+// 	}
+// }
+
+// func twoSum(nums []int, target int) []int {
+// 	l := 0
+// 	r := len(nums) - 1
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[l]+nums[r] < target {
+// 			l++
+// 		} else if nums[l]+nums[r] > target {
+// 			r--
+// 		} else {
+// 			return []int{l, r}
+// 		}
+// 	}
+// 	return nil
+// }
+
 package main
 
 import "fmt"
@@ -40,7 +70,7 @@ func main() {
 	target := 10
 	result := twoSum(nums, target)
 	if result == nil {
-		fmt.Println("there is not any two pair of array whose sum is equal to the target...")
+		fmt.Println("There is not any 2 numbers i spresent whose sum is equal to target...")
 	} else {
 		fmt.Println("Indices of numbers : ", result)
 	}
