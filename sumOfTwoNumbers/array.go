f5ca50f1ce995eeed7bd3b860692ae863510ea8b
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-10 23:16:56 +0530

    error solved

--- sumOfTwoNumbers/array.go
+++ sumOfTwoNumbers/array.go
@@ -61,6 +61,36 @@
 // 	return nil
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	nums := []int{2, 3, 5, 7, 11}
+// 	target := 10
+// 	result := twoSum(nums, target)
+// 	if result == nil {
+// 		fmt.Println("There is not any 2 numbers i spresent whose sum is equal to target...")
+// 	} else {
+// 		fmt.Println("Indices of numbers : ", result)
+// 	}
+// }
+
+// func twoSum(nums []int, target int) []int {
+// 	l := 0
+// 	r := len(nums) - 1
+// 	for l < r {
+// 		if nums[l]+nums[r] < target {
+// 			l++
+// 		} else if nums[l]+nums[r] > target {
+// 			r--
+// 		} else {
+// 			return []int{l, r}
+// 		}
+// 	}
+// 	return nil
+// }
+
 package main
 
 import "fmt"
@@ -70,7 +100,7 @@ func main() {
 	target := 10
 	result := twoSum(nums, target)
 	if result == nil {
-		fmt.Println("There is not any 2 numbers i spresent whose sum is equal to target...")
+		fmt.Println("there isnot any 2 numbers is present whose sum is equals to target...")
 	} else {
 		fmt.Println("Indices of numbers : ", result)
 	}
@@ -79,14 +109,5 @@ func main() {
 func twoSum(nums []int, target int) []int {
 	l := 0
 	r := len(nums) - 1
-	for i := 0; i < len(nums); i++ {
-		if nums[l]+nums[r] < target {
-			l++
-		} else if nums[l]+nums[r] > target {
-			r--
-		} else {
-			return []int{l, r}
-		}
-	}
-	return nil
+
 }
