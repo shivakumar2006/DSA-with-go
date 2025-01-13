4a0a8eaa0734d94de8c768d38ca3054af9a103f8
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-13 23:45:50 +0530

    saved changes

--- longestSubStringWithoutRepeatingCharacters/longestSubString.go
+++ longestSubStringWithoutRepeatingCharacters/longestSubString.go
@@ -1,35 +1,71 @@
 // given a string s. find the length of the longest substring without repeaing characters.
 
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func max(a int, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
+// func max(a int, b int) int {
+// 	if a > b {
+// 		return a
+// 	}
+// 	return b
+// }
 
-func main() {
-	s := "abcabcbb"
-	fmt.Println(longestSubString(s))
-}
+// func main() {
+// 	s := "abcabcbb"
+// 	fmt.Println(longestSubString(s))
+// }
 
-func longestSubString(s string) int {
-	hashMap := make(map[byte]int)
+// func longestSubString(s string) int {
+// 	hashMap := make(map[byte]int)
 
-	maxLength := 0
-	start := 0
+// 	maxLength := 0
+// 	start := 0
 
-	for i := 0; i < len(s); i++ {
-		if index, found := hashMap[s[i]]; found && index >= start {
-			start = index + 1
-		}
+// 	for i := 0; i < len(s); i++ {
+// 		if index, found := hashMap[s[i]]; found && index >= start {
+// 			start = index + 1
+// 		}
 
-		hashMap[s[i]] = i
+// 		hashMap[s[i]] = i
 
-		maxLength = max(maxLength, i-start+1)
-	}
-	return maxLength
-}
+// 		maxLength = max(maxLength, i-start+1)
+// 	}
+// 	return maxLength
+// }
+
+// package main
+
+// import "fmt"
+
+// func max(a int, b int) int {
+// 	if a > b {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// func main() {
+// 	s := "abcabcbb"
+// 	fmt.Println(longestSubString(s))
+// }
+
+// func longestSubString(s string) int {
+// 	hashMap := make(map[byte]int)
+
+// 	maxLength := 0
+// 	start := 0
+
+// 	for i := 0; i < len(s); i++ {
+// 		if index, found := hashMap[s[i]]; found && index >= start {
+// 			start = index + 1
+// 		}
+
+// 		hashMap[s[i]] = i
+
+// 		maxLength = max(maxLength, i-start+1)
+// 	}
+// 	return maxLength
+// }
+
+
