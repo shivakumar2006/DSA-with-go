1bff6adbefdaca13e61acfd1de02d3d5f33cc899
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-13 22:55:14 +0530

    solved

--- countNumberOfNiceSubArray/countNum.go
+++ countNumberOfNiceSubArray/countNum.go
@@ -136,6 +136,34 @@
 // 	return result
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	nums := []int{1, 1, 2, 1, 1}
+// 	k := 3
+// 	fmt.Println(numberOfSubArray(nums, k))
+// }
+
+// func numberOfSubArray(nums []int, k int) int {
+// 	result := 0
+// 	current := 0
+// 	hashMap := make(map[int]int)
+// 	hashMap[0] = 1
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[i]%2 != 0 {
+// 			current++
+// 		}
+
+// 		if value, exist := hashMap[current-k]; exist {
+// 			result += value
+// 		}
+// 		hashMap[current]++
+// 	}
+// 	return result
+// }
+
 package main
 
 import "fmt"
